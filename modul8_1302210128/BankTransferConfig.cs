c80ba0d7c13e36b278ceea7f913e0de3cba5820c
Leirannt <93717185+Leirannt@users.noreply.github.com>
2023-04-12 17:38:29 +0700

    class BankTransferConfig

---
+++ modul8_1302210128/BankTransferConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace modul8_1302210128
+{
+
+
+    class BankTransferConfig
+    {
+        private static string FilePath = @"D:\vscode\C#\modul8_1302210128\modul8_1302210128\bank_transfer_config.json";
+        private static dynamic config;
+        public static void loadConfig()
+        {
+            string ConfigJson = File.ReadAllText(FilePath);
+            config = JsonConvert.DeserializeObject(configJson);
+            config = new JObject();
+            config.lang = "en";
+            config.transfer = new JObject();
+            config.transfer.threshold = 25000000;
+            config.transfer.low_fee = 6500;
+            config.transfer.high_fee = 15000;
+            config.methods = new JArrray { "RTO (real-time)", "SKN", "RTGS", "BI FAST" };
+            config.confirmation = new JObject();
+            config.confirmation.en = "yes";
+            config.confirmation.id = "ya";
+        }
+
+        public static string GetLang()
+        {
+            return config.lang;
+        }
+
+        public static int GetThreshold()
+        {
+            return config.transfer.threshold;
+        }
+
+        public static int GetLowFee()
+        {
+            return config.transfer.low_fee;
+        }
+        public static int GetHighFee()
+        {
+            return config.transfer.high_fee;
+        }
+        public static List<string> GetMethods()
+        {
+            return config.methods.ToObject<List<string>>();
+        }
+        public static string GetConfirmation(string lang)
+        {
+            return config.confirmation[lang];
+        }
+    }
+}
+
+
--- modul8_1302210128/Program.cs
+++ modul8_1302210128/Program.cs
@@ -6,7 +6,43 @@ namespace modul8_1302210128
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankTransferConfig.LoadConfig();
+        
+            string lang = BankTransferConfig.GetLang();
+
+            if ( lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+            else if (lang == "id")
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            }
+
+            int jumlah = int.Parse(Console.ReadLine());
+            int biayaTF;
+
+            if (jumlah <= BankTransferConfig.GetThreshold())
+            {
+                biayaTF = BankTransferConfig.GetLowFee();
+            }
+            else 
+            {
+                biayaTF = BankTransferConfig.GetHighFee();
+            }
+
+            int nominal = jumlah + biayaTF;
+
+            if (BankTransferConfig.GetLang() == "en")
+            {
+                Console.WriteLine($"TF Fee = { biayaTF}\ntotal amount = {nominal}");
+            }
+            else
+            {
+                Console.WriteLine($"Biaya TF = { biayaTF}\ntotal biaya = {nominal}");
+            }
+
         }
+        
     }
 }
